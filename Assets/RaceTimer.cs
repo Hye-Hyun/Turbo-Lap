88a739ffd224b9a6dcf1932f1bc5bfc47f5c3043
Hye-Hyun <lovewinner0822@gmail.com>
2026-06-16 19:26:57 +0900

    Add race timer

---
+++ Assets/RaceTimer.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using TMPro;
+
+public class RaceTimer : MonoBehaviour
+{
+    public TextMeshProUGUI timerText;
+
+    private float elapsedTime;
+    private bool isRunning = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isRunning) return;
+
+        elapsedTime += Time.deltaTime;
+
+        int minutes = Mathf.FloorToInt(elapsedTime / 60);
+        float seconds = elapsedTime % 60;
+
+        timerText.text = string.Format("{0:00}:{1:00.00}", minutes, seconds);
+    }
+
+    public void StopTImer()
+    {
+        isRunning = false;
+    }
+}
