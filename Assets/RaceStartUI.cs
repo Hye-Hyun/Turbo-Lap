b821238c4109fd4d24d31b69875cfcd1bf6d3d31
Hye-Hyun <lovewinner0822@gmail.com>
2026-06-19 01:27:11 +0900

    Add engine sound

--- Assets/RaceStartUI.cs
+++ Assets/RaceStartUI.cs
@@ -8,7 +8,11 @@ public class RaceStartUI : MonoBehaviour
     [SerializeField] private GameObject count2UI;
     [SerializeField] private GameObject count1UI;
     [SerializeField] private GameObject startUI;
+
     [SerializeField] private RaceTimer raceTimer;
+
+    [SerializeField] private AudioSource startAudio;
+    [SerializeField] private AudioSource engineAudio;
 
     public static bool raceStarted = false; //차량 움직임 방지 
 
@@ -32,13 +36,18 @@ public class RaceStartUI : MonoBehaviour
         yield return new WaitForSeconds(1f);
         count2UI.SetActive(false);
 
+        startAudio.Play(); //엔진음 재생 
+
         count1UI.SetActive(true);
         yield return new WaitForSeconds(1f);
         count1UI.SetActive(false);
 
         startUI.SetActive(true);
+        engineAudio.Play(); //엔진음 재생
         yield return new WaitForSeconds(1f);
         startUI.SetActive(false);
+
+        engineAudio.Play(); //엔진음 재생
 
         raceStarted = true; //카운트다운 이후 차량 움직임
         raceTimer.StartTimer(); //카운트다운 이후 타이머 동작
